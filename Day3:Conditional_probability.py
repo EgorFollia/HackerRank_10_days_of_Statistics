5cf6807eab2dc658e98bcd33c9cf7060b8425300
EgorFollia <egorfollia@gmail.com>
2019-01-26 18:36:24 +0300

    Day 3 completed

---
+++ Day3:Conditional_probability.py
@@ -0,0 +1,9 @@
+In this task we have 4 different outcomes:
+	B	G
+B	BB	GB
+G	BG	GG
+
+And we also have one condition, that is one child is a boy
+Thereby, one of three outcomes takes place:
+	P(b) = 1/3
+
